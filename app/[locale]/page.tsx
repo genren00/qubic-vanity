22b8a295fb39ead9292b015943f6d9f2d94655ec
genren00 <fidynop@gmail.com>
2025-09-07 17:35:46 +0300

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from 'next/navigation'
- 
-export default function IndexPage() {
-  redirect('/home')
+'use client'
+
+import Home from './home/page'  // Import your home page
+
+export default function HomePage() {
+  return <Home />
 }
